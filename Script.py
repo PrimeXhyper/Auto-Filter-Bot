b5de576ddc67a38fa7547f88ae89631e3ba53831
𝙋𝙧𝙞𝙢𝙚 𝙀𝙧𝙖 ⤸ 🥀𓂃 <susantasarkar123256m@gmail.com>
2026-03-27 01:19:24 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,7 +4,7 @@ class script(object):
 ɪ ᴀᴍ ᴀɴ ᴀᴅᴠᴀɴᴄᴇᴅ ᴀᴜᴛᴏ ꜰɪʟᴛᴇʀ ʙᴏᴛ.
 ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴍᴏᴠɪᴇꜱ, ꜱᴇʀɪᴇꜱ, ᴀɴɪᴍᴇꜱ, ᴀɴᴅ ᴍᴜᴄʜ ᴍᴏʀᴇ... 🎬
 ᴊᴜꜱᴛ ꜱᴇɴᴅ ᴍᴇ ᴛʜᴇ ɴᴀᴍᴇ ᴏꜰ ᴛʜᴇ ᴍᴏᴠɪᴇ/ꜱᴇʀɪᴇꜱ ʏᴏᴜ ᴡᴀɴᴛ. 🔍</b>
-<blockquote>🔰 𝙋𝙤𝙬𝙚𝙧𝙚𝙙 𝘽𝙮: <a href="https://t.me/DragonByte_Network">𝘋𝘳𝘢𝘨𝘰𝘯𝘉𝘺𝘵𝘦</a></blockquote>"""
+<blockquote>🔰 𝙋𝙤𝙬𝙚𝙧𝙚𝙙 𝘽𝙮: <a href="https://t.me/DragonByte_Network">𝘋𝘳𝘢𝘨𝘰𝘯𝘉𝘺𝘵𝘦 𝘕𝘦𝘵𝘸𝘰𝘳𝘬 !!</a></blockquote>"""
 
     FEATURES_TXT = """<b>🛠️ ʜᴇʀᴇ ᴀʀᴇ ᴍʏ ᴀᴠᴀɪʟᴀʙʟᴇ ꜰᴇᴀᴛᴜʀᴇꜱ 🛠️</b>"""
 
