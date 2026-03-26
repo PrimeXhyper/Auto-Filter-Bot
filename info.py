8394fdb39634090c798e58272dd5ba719ac9b102
𝙋𝙧𝙞𝙢𝙚 𝙀𝙧𝙖 ⤸ 🥀𓂃 <susantasarkar123256m@gmail.com>
2026-03-27 01:48:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -147,7 +147,7 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else "https://{}/".format(FQDN, PORT)
+URL = "alternative-kathe-thehyper333-eb56e5e6.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else "alternative-kathe-thehyper333-eb56e5e6.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', '𝘚𝘤𝘳𝘪𝘱𝘵𝘍𝘭𝘪𝘹'))
@@ -159,11 +159,11 @@ if 'DYNO' in environ:
     APP_NAME = str(getenv('APP_NAME'))
 else:
     ON_HEROKU = False
-HAS_SSL = bool(getenv('HAS_SSL', False))
+HAS_SSL = bool(getenv('HAS_SSL', True))
 if HAS_SSL:
-    URL = "https://alternative-kathe-thehyper333-eb56e5e6.koyeb.app/".format(FQDN)
+    URL = "alternative-kathe-thehyper333-eb56e5e6.koyeb.app/".format(FQDN)
 else:
-    URL = "https://alternative-kathe-thehyper333-eb56e5e6.koyeb.app/".format(FQDN)
+    URL = "alternative-kathe-thehyper333-eb56e5e6.koyeb.app/".format(FQDN)
 
 
 REACTIONS = ["🤝", "😇", "🤗", "😍", "👍", "🎅", "😐", "🥰", "🤩", "😱", "🤣", "😘", "👏", "😛", "😈", "🎉", "⚡️", "🫡", "🤓", "😎", "🏆", "🔥", "🤭", "🌚", "🆒", "👻", "😁"]
